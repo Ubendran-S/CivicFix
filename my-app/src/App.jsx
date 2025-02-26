65dbaedf072f189a9e44022f05add1a57e7ab141
Ubendran-S <ubendran1611@gmail.com>
2025-02-26 21:46:34 +0530

    msg

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
     <div>
       <Router>
       <Routes>
-        <Route path='/' element={ <Login/>}/>
-        <Route path='/home' element={<Home />}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/home' element={<Home/>}/>
         <Route path='/report' element={<Report/>}/>
-        <Route path='/signup' element={ <SignUp/>}/>
-        <Route path='/thankyou' element={ <Thank/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/thankyou' element={<Thank/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/myreports' element={<MyReport/>}/>
       </Routes>
