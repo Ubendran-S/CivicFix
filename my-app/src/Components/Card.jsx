bd23a5ec39715e88e192e8097b955dd352ba19ec
Ubendran-S <ubendran1611@gmail.com>
2025-03-22 10:10:07 +0530

    msg

--- my-app/src/Components/Card.jsx
+++ my-app/src/Components/Card.jsx
@@ -3,12 +3,12 @@ import './Card.css';
 import loc from '../Pages/location.png'
 const Card = (props) => {
   return (
-    <div className='card' style={{backgroundImage:`url(${props.image})`,backdropFilter: "blur(5px)",}}>
-        <p className='c-main' >{props.name}</p><hr className='ju'/>
+    <div className='card' style={{backgroundImage:`url(${props.image})`}}>
+        <p className='c-main' style={{color:"white"}}>{props.name}</p><hr className='ju'/>
         <div style={{display:"flex",gap:"10px"}}>
-          <p style={{fontSize:"18px"}}>{props.location}</p>
+          <p style={{fontSize:"18px",color:"white"}} >{props.location}</p>
         </div>
-        <p className='progress'style={{backgroundColor:props.color}}>{props.status}</p>
+        <p className='progress'style={{color:props.color}}>{props.status}</p>
     </div>
   )
 }
--- my-app/src/Pages/Contact.jsx
+++ my-app/src/Pages/Contact.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
 import Nav from '../Components/Navbar'
 import Footer from './Footer'
-import dra from './threedragon.jpeg'
 import './Contact.css'
+import loc from './location_.png'
+import em from './email.png'
+import ph from './phone.png'
 const Contact = () => {
   return (
     <div>
       <Nav />
       <div style={{display:"flex",justifyContent:"center"}}>
         <div className='pol'>
-          <h1 style={{color:"black"}}>CivicFix Developers:</h1>
-          <ul>
-            <li style={{ WebkitTextStroke: "2px black", letterSpacing: "10px" }}>Maatu Ravi</li>
-            <li style={{ WebkitTextStroke: "2px black", letterSpacing: "10px" }}>Saara Paambu</li>
-            <li style={{ WebkitTextStroke: "2px black", letterSpacing: "10px" }}>Poona mani</li>
-          </ul>
-        </div>
-        <div>
-          <img src={dra} className='do'/>
+            <div className='c-jk'>
+                <h1 style={{color:"black",display:"flex",justifyContent:"center"}}>Get in Touch</h1>
+                <div className='h-jk'>
+                <label>Name</label>
+                <input></input>
+                <label>Email</label>
+                <input></input>
+                <label>Message</label>
+                <textarea></textarea>
+                <button>Submit</button>
+                </div>
+            </div>
+            <div className='d-jk'>
+                <div style={{display:"flex",alignItems:"center",gap:"1vw"}}>
+                <img src={loc} style={{height:"8vh",width:"4vw"}}/>
+                <h2>Address</h2>
+                </div>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;123,Vivekanandhar street,Dubai kurukusandhu,Dubai</p>
+                <div style={{display:"flex",alignItems:"center",gap:"1vw"}}>
+                <img src={ph} style={{height:"8vh",width:"4vw"}}/>
+                <h2>Phone</h2>
+                </div>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+91 1234567890</p>   
+                <div style={{display:"flex",alignItems:"center",gap:"1vw"}}>
+                <img src={em} style={{height:"8vh",width:"4vw"}}/>
+                <h2>Email</h2>
+                </div>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;civicfix@gmail.com</p>   
+            </div>
         </div>
       </div>
       <Footer/>
